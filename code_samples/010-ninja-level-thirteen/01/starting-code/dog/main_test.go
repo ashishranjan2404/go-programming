e939d708a1fc451c09c49b4a72fbcf543906f681
Ashish Ranjan <ashiranj@cisco.com>
2020-10-18 23:18:20 -0700

    BET for package dog

---
+++ code_samples/010-ninja-level-thirteen/01/starting-code/dog/main_test.go
@@ -0,0 +1,57 @@
+package dog
+
+import (
+"fmt"
+"testing"
+)
+
+func TestYears(t *testing.T) {
+	s:= 7
+	ans:= 49
+	s = Years(s)
+	if ans != s{
+		t.Error("got", s, "expected",ans)
+	}
+}
+
+func TestYearsTwo(t *testing.T) {
+	s:= 7
+	ans:= 49
+	s = YearsTwo(s)
+	if ans != s{
+		t.Error("got", s, "expected",ans)
+	}
+}
+
+func ExampleYears() {
+	s := 7
+	xs := Years(s)
+	fmt.Println(xs)
+	// Output :
+	// 49
+}
+
+func ExampleYearsTwo() {
+	s := 7
+	xs := YearsTwo(s)
+	fmt.Println(xs)
+	// Output :
+	// 49
+}
+
+func BenchmarkYears(b *testing.B) {
+	xs := 7
+	b.ResetTimer()
+	for i:= 0; i<b.N; i++ {
+		Years(xs)
+	}
+}
+
+
+func BenchmarkYearsTwo(b *testing.B) {
+	xs := 7
+	b.ResetTimer()
+	for i:= 0; i<b.N; i++ {
+		YearsTwo(xs)
+	}
+}
